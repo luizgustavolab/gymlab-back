4abc0f6e876aed6eff4d16504447d7e58367c9a5
Luiz Gustavo <luizgustavolab@gmail.com>
2026-06-01 13:23:25 -0300

    feat: health controller

---
+++ src/main/java/com/gymlab/api/HealthController.java
@@ -0,0 +1,12 @@
+package com.gymlab.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
